f26d111e567f4ece31758fd5a2aa4d6e95818171
ski12345491 <abu@ascinatetech.com>
2026-03-12 17:28:38 +0530

    first comit

---
+++ controllers/authController.js
@@ -0,0 +1,144 @@
+import supabase from "../config/supabase.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { generateOTP } from "../utils/otp.js";
+
+export const register = async (req, res) => {
+  try {
+    const { name, email, phone, password, bio, gender, age, latitude, longitude} = req.body;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const { data, error } = await supabase
+      .from("users")
+      .insert([{ name, email, phone, password: hashedPassword, bio, gender, age, latitude, longitude }])
+      .select()
+      .single();
+
+    if (error) return res.status(400).json(error);
+
+    res.json({
+      message: "User registered successfully",
+      user: data
+    });
+
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const { data: user, error } = await supabase
+      .from("users")
+      .select("*")
+      .eq("email", email)
+      .single();
+
+    if (error || !user) {
+      return res.status(400).json({ message: "User not found" });
+    }
+
+    const match = await bcrypt.compare(password, user.password);
+
+    if (!match) {
+      return res.status(401).json({ message: "Invalid password" });
+    }
+
+    const token = jwt.sign(
+      { id: user.id },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+
+    res.json({
+      message: "Login successful",
+      token
+    });
+
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+export const logout = async (req, res) => {
+  res.json({ message: "Logout success (client remove token)" });
+};
+
+export const sendOtp = async (req, res) => {
+  try {
+    const { email } = req.body;
+
+    const otp = generateOTP();
+
+    await supabase
+      .from("otp_codes")
+      .insert([{ email, otp }]);
+
+    res.json({
+      message: "OTP sent",
+      otp
+    });
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+export const verifyOtp = async (req, res) => {
+  try {
+    const { email, otp } = req.body;
+
+    const { data } = await supabase
+      .from("otp_codes")
+      .select("*")
+      .eq("email", email)
+      .eq("otp", otp)
+      .single();
+
+    if (!data) {
+      return res.status(400).json({ message: "Invalid OTP" });
+    }
+
+    res.json({ message: "OTP verified" });
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+export const forgotPassword = async (req, res) => {
+  const { email } = req.body;
+
+  const otp = generateOTP();
+
+  await supabase
+    .from("otp_codes")
+    .insert([{ email, otp }]);
+
+  res.json({
+    message: "OTP sent for password reset",
+    otp
+  });
+};
+
+export const resetPassword = async (req, res) => {
+  const { email, password } = req.body;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const { error } = await supabase
+    .from("users")
+    .update({ password: hashedPassword })
+    .eq("email", email);
+
+  if (error) {
+    return res.status(400).json(error);
+  }
+
+  res.json({
+    message: "Password reset successful"
+  });
+};
---
+++ routes/authRoutes.js
@@ -0,0 +1,22 @@
+import express from "express";
+import {
+  register,
+  login,
+  logout,
+  sendOtp,
+  verifyOtp,
+  forgotPassword,
+  resetPassword
+} from "../controllers/authController.js";
+
+const router = express.Router();
+
+router.post("/register", register);
+router.post("/login", login);
+router.post("/logout", logout);
+router.post("/send-otp", sendOtp);
+router.post("/verify-otp", verifyOtp);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password", resetPassword);
+
+export default router;
