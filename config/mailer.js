1b03865f6f72d349c809b90195eae3a8c91f4d4a
ski12345491 <abu@ascinatetech.com>
2026-03-13 18:21:31 +0530

    email done

--- config/mailer.js
+++ config/mailer.js
@@ -4,7 +4,10 @@ const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
-  family: 4,
+  family: 4, // force IPv4
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
