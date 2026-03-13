c713652cef2cb907b7ad1d882b3ec97e06f33195
ski12345491 <abu@ascinatetech.com>
2026-03-13 18:29:13 +0530

    last push done

--- config/mailer.js
+++ config/mailer.js
@@ -1,13 +1,7 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  family: 4, // force IPv4
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
+service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
