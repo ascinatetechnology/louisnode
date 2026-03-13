159684702460899f2f8e414c978199b45eaf8c02
ski12345491 <abu@ascinatetech.com>
2026-03-13 14:56:32 +0530

    first push

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import path from "path";
 import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.set("view engine", "ejs");
+app.set("views", path.join(process.cwd(), "views"));
+
+app.get("/", (req, res) => {
+  res.render("login");
+});
 
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
