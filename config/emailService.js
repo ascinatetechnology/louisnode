a22d9fe2997aa4bd359cb954ef3ad9bbd6484cb9
ski12345491 <abu@ascinatetech.com>
2026-03-27 12:46:00 +0530

    mail done

--- config/emailService.js
+++ config/emailService.js
@@ -1,36 +1,38 @@
-import nodemailer from "nodemailer";
+import SibApiV3Sdk from "sib-api-v3-sdk";
+
+const client = SibApiV3Sdk.ApiClient.instance;
+const apiKey = client.authentications["api-key"];
+
+apiKey.apiKey = process.env.BREVO_API_KEY;
 
 export const sendEmail = async (to, otp) => {
   try {
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-      }
-    });
+    const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
 
-    const mailOptions = {
-      from: `"Louis App" <${process.env.EMAIL_USER}>`,
-      to,
+    const sendSmtpEmail = {
+      sender: {
+        email: "devski756@gmail.com",
+        name: "Louis App",
+      },
+      to: [{ email: to }],
       subject: "Your OTP Code",
-      html: `
+      htmlContent: `
         <div style="font-family:sans-serif">
           <h2>Louis App</h2>
           <p>Your OTP is:</p>
           <h1>${otp}</h1>
           <p>This OTP will expire in 5 minutes</p>
         </div>
-      `
+      `,
     };
 
-    const info = await transporter.sendMail(mailOptions);
+    const response = await apiInstance.sendTransacEmail(sendSmtpEmail);
 
-    console.log("✅ Email sent:", info);
-    return info;
+    console.log("✅ Brevo Email sent:", response);
+    return response;
 
   } catch (err) {
-    console.error("🔥 Nodemailer Error:", err);
+    console.error("🔥 Brevo Error:", err);
     throw err;
   }
 };
